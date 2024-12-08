d782d8c72188814b3e476bfc04013eb26055b762
Andrew Duncan <arduncan@bigpond.com>
2024-12-09 09:18:04 +1100

    Fix wrong type for round

--- raspinfo/raspinfo.cxx
+++ raspinfo/raspinfo.cxx
@@ -434,7 +434,7 @@ main(
 
             fb->update();
 
-            const auto nextSecond = std::chrono::round<Sec>(now) + 1s;
+            const auto nextSecond = std::chrono::round<std::chrono::seconds>(now) + 1s;
             std::this_thread::sleep_until(nextSecond);
         }
 
